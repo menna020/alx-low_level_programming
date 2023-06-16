06759451a3fb81196313869f0418af29cf0fd95b
menna020 <mennawork20@gmail.com>
2023-06-15 18:24:17 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - printing
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
