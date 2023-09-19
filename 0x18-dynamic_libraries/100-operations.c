3a1790687ca97c269159e8929128a68c83ab7e44
menna_tullah_mohamed <mennawork20@gmail.com>
2023-09-18 18:22:31 -0700

    dynamic

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+
+int div(int x, int y)
+{
+	if (y == 0){
+	printf ("erorr\n");
+		return (0);
+	}
+	return (x / y);
+}
+
+
+int mod(int x, int y)
+{
+	if (y == 0) {
+		printf ("erorr\n");
+		return (0);
+	}
+	return (x % y);
+}
---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -p /tmp https://github.com/menna020/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so export LD_PRELOAD=/tmp/libgiga.so
