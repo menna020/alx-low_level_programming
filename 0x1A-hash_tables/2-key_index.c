2c765e4ae8dc9d6723c0b1d996fbfbf93613c1b3
menna_tullah_mohamed <mennawork20@gmail.com>
2023-10-11 11:37:55 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,17 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Associate a index to a key.
- * @key: Key to be indexed.
- * @size: Size of the hash table.
+ * key_index - gives the index of a key
+ * @key: key to get index for
+ * @size: size of the hash table
  *
- * Return: Int after the operations.
+ * Return: index for the key
  */
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int result;
-
-	result = hash_djb2(key) % size;
-	return (result);
+	return (hash_djb2(key) % size);
 }
